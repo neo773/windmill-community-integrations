94e7a8a74d1727b73578fc09cfde2be8fca2560f
neo773 <62795688+neo773@users.noreply.github.com>
2024-04-14 02:13:43 +0530

    fix test

--- integrations/todoist/Invite-User-To-Project/script.bun.test.ts
+++ integrations/todoist/Invite-User-To-Project/script.bun.test.ts
@@ -2,7 +2,6 @@ import { describe, it, expect } from 'bun:test'
 
 describe('Invite User To Project', () => {
 	it('should perform the integration action', async () => {
-		e
 		expect(true).toBeTruthy()
 	})
 })
